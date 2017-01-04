31cd1cae1778a44b54167e765b08b698cb9d6fe9
kalaiselvinatarajan <kalaiselvi.natarajan@gmail.com>
2017-01-03 20:30:06 -0600

    Add files via upload

---
+++ APlusB.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class APlusB {
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+    int a = s.nextInt();
+    int b = s.nextInt();
+	if(a<0 || b<0 || a>9 || b>9){
+		 main(args);
+	}
+	else{
+    System.out.println(a + b);
+	}
+  }
+}
